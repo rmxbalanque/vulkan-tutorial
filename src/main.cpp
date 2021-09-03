11a19a88dfed529b24aafa9fab9633f5cca60323
Roland Munguia <rmxbalanque@gmail.com>
2021-09-02 21:29:26 -0700

    [+] GLFW Window Running

--- src/main.cpp
+++ src/main.cpp
@@ -4,33 +4,96 @@
 
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
+
 #include <glm/vec4.hpp>
 #include <glm/mat4x4.hpp>
 
 #include <iostream>
 
-int main() {
-    glfwInit();
+class HelloTriangleApplication
+{
+public:
+	void run()
+	{
+		initWindow();
+		initVulkan();
+		mainLoop();
+		cleanup();
+	}
 
-    glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+private:
 
-    uint32_t extensionCount = 0;
-    vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+	GLFWwindow* window = nullptr;
+	const uint32_t wWidth = 1080;
+	const uint32_t wHeight = 720;
 
-    std::cout << extensionCount << " extensions supported\n";
+	void initWindow()
+	{
+		glfwInit();										// Init GLFW lib
+		glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);	// We are not using OpenGL
+		glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);		// No window resize (for now)
 
-    glm::mat4 matrix;
-    glm::vec4 vec;
-    auto test = matrix * vec;
+		// Create window
+		window = glfwCreateWindow(wWidth, wHeight, "Vulkan", nullptr, nullptr);
+	}
 
-    while(!glfwWindowShouldClose(window)) {
-        glfwPollEvents();
-    }
+	void initVulkan()
+	{
 
-    glfwDestroyWindow(window);
+	}
 
-    glfwTerminate();
+	void mainLoop()
+	{
+		while (!glfwWindowShouldClose(window)) {
+			glfwPollEvents();
+		}
+	}
 
-    return 0;
+	void cleanup()
+	{
+		glfwDestroyWindow(window);	// Destroy native window
+		glfwTerminate();			// Deinit GLFW library
+	}
+};
+
+int main()
+{
+	HelloTriangleApplication app;
+
+	try
+	{
+		app.run();
+	}
+	catch (const std::exception& e)
+	{
+		std::cerr << e.what() << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	return EXIT_SUCCESS;
+
+	glfwInit();
+
+	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+	GLFWwindow* window = glfwCreateWindow(800, 600, "Vulkan window", nullptr, nullptr);
+
+	uint32_t extensionCount = 0;
+	vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+	std::cout << extensionCount << " extensions supported\n";
+
+	glm::mat4 matrix;
+	glm::vec4 vec;
+	auto test = matrix * vec;
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
+
+	return 0;
 }
